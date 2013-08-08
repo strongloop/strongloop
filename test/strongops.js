076895db7c6c8fc2ddebdd6ba4f06555e68b6d2c
Edmond Meinfelder <edmond@strongloop.com>
2013-08-08 14:44:17 -0700

    Implement the 'slc strongops' command
    This command registers users with StrongOps. If no options are used to
    specify user name, email or password, the command will prompt users for
    the data which is validated. The StrongOps credentials can be stored in
    ~/strongops.json, ./strongops.json or ./package.json in any combination,
    including all or none.

---
+++ test/strongops.js
@@ -0,0 +1,93 @@
+var assert = require('assert');
+var is = require('is2');
+var strops = require('../lib/commands/strongops');
+var test = require('./config').strongops;
+
+describe('getNpmEmail', function() {
+  it('Should return the email string from ~/.npmrc', function() {
+    assert.equal(test.getNpmEmail, strops.test.getNpmEmail());
+  });
+});
+
+describe('getGitConfigInfo', function() {
+  it('Should return the user name and email string from ~/.gitconfig', function() {
+    var git = strops.test.getGitConfigInfo();
+    assert.deepEqual(git, strops.test.getGitConfigInfo());
+  });
+});
+
+describe('getUserHome', function() {
+  it('Should return the home directory for the user', function() {
+    assert.equal(test.getNpmEmail, strops.test.getNpmEmail());
+  });
+});
+
+// FIXME: make another test with a config, to test .npmrc email retrieval
+describe('getDefaults', function() {
+  it('Should get the defaults from ~/.gitconfig and possibly ~/.npmrc', function() {
+    assert.deepEqual(test.getDefaults, strops.test.getDefaults());
+  });
+});
+
+describe('getCmdLineOverrides', function() {
+  it('Given an empty options object, should an empty object', function() {
+    assert.deepEqual({}, strops.test.getCmdLineOverrides());
+  });
+});
+
+describe('getCmdLineOverrides', function() {
+  it('Given an options with a name property, should an object with a name property', function() {
+    var options = { name: 'E. Meinfelder' };
+    assert.deepEqual({ name: 'E. Meinfelder' }, strops.test.getCmdLineOverrides(options));
+  });
+});
+
+describe('getCmdLineOverrides', function() {
+  it('Given options with an email property, should an object with an email property', function() {
+    var options = { email: 'e@e.com' };
+    assert.deepEqual({ email: 'e@e.com' }, strops.test.getCmdLineOverrides(options));
+  });
+});
+
+describe('getCmdLineOverrides', function() {
+  it('Given options with a password property, should an object with a password and password2 properties', function() {
+    var options = { password: '12345678' };
+    var expected = { password: '12345678', password2: '12345678' };
+    assert.deepEqual(expected, strops.test.getCmdLineOverrides(options));
+  });
+});
+
+describe('getCmdLineOverrides', function() {
+  it('Given options with a name, email & password, should an object with name, email, password and password2 properties', function() {
+    var options = {
+      name: 'Monkey',
+      email: 'monkey@monkey.com',
+      password: '12345678' };
+    var expected = {
+      name: 'Monkey',
+      email: 'monkey@monkey.com',
+      password: '12345678',
+      password2: '12345678' };
+    assert.deepEqual(expected, strops.test.getCmdLineOverrides(options));
+  });
+});
+
+describe('getCmdLineOverrides', function() {
+  it('Given options with an email property, should an object with an email property', function() {
+    var options = { email: 'e@e.com' };
+    assert.deepEqual({ email: 'e@e.com' }, strops.test.getCmdLineOverrides(options));
+  });
+});
+
+describe('getFileSync', function() {
+  it('Should return false when getting a file that is not present.', function() {
+    assert.ok(strops.test.getFileSync('./dhsjdhjhdjdww78783') === false);
+  });
+});
+
+describe('getFileSync', function() {
+  it('Should return the contents of a file when file is present.', function() {
+    assert.ok(is.nonEmptyStr(strops.test.getFileSync(test.getFileSync)) === true);
+  });
+});
+
