strongloop/strongloop

commit cf9593cdf1cd8568ac804a107345df01ed53c8d1

Include only types in debug statement, not values
Values include the value of the usage string, which overwhelm the debug
output.

lib/loader.js
@@ -189,7 +189,7 @@ function getRun(name) {
   var self = this;
   var module = self.loadCommand(name);
 
-  debug('loadCommand', name, module);
+  debug('loadCommand', 'name', name, 'run?', typeof module.run, 'usage?', typeof module.usage);
 
   return module ? module.run : null;
 }

commit 230bb8904c10fdb67983e565a7c7e2fcb2baaf5d

lib/loader: fixed bug introduced by cf9593c
Fixed lib/loader to handle commands with no run/usage attribute without
crashing. The change was required to make `slc install` work in unit-tests.

lib/loader.js
@@ -189,7 +189,9 @@ function getRun(name) {
   var self = this;
   var module = self.loadCommand(name);
 
-  debug('loadCommand', 'name', name, 'run?', typeof module.run, 'usage?', typeof module.usage);
+  debug('loadCommand', 'name', name,
+    'run?', module && typeof module.run,
+    'usage?', module && typeof module.usage);
 
   return module ? module.run : null;
 }