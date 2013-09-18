233e96fa775f9f56cc59cf8a8769aaa605a94f3e
Sam Roberts <sam@strongloop.com>
2013-09-17 21:22:03 -0700

    Remove .git after cloning example

---
+++ test/example.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+
+var sandbox = require('./helpers/sandbox.js');
+var spawnCliInSandbox = require('./helpers/runner.js').spawnCliInSandbox;
+
+describe('suite example', function() {
+  this.timeout(5000);
+
+  before(function(done) {
+    sandbox.reset();
+    spawnCliInSandbox(['example', 'suite', '--no-install'])
+      .run(done);
+  });
+
+  it('does not leave a .git folder', function () {
+    var dotGit = sandbox.path('sls-sample-app', '.git', 'config');
+    assert(!fs.existsSync(dotGit));
+  });
+});
