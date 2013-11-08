4b8db7fd270436151fb859019ea141720c172ad0
Sam Roberts <sam@strongloop.com>
2013-11-08 15:05:50 -0800

    Quiet script down, add some readable messages

--- prepublish.sh
+++ prepublish.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-set -x
 set -e
 
+echo Updating example dependencies
 (cd node_modules/sls-sample-app; npm update)
 (cd node_modules/sn-example-blog; npm update)
 (cd node_modules/sn-example-chat; npm update)
 (cd node_modules/sn-example-urlsaver; npm update)
 
+echo Prune oracle connector
 find node_modules -depth -name loopback-connector-oracle -exec rm -rf {} \;
+echo Prune test directories of dependent modules
 find node_modules -depth -type d -regex '.*/node_modules/[^/]*/test' -exec rm -rf {} \;
+
