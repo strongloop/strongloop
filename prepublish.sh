44c9a8cdd9de9459998491d98c72b644b23b68b3
Sam Roberts <sam@strongloop.com>
2014-12-31 11:29:26 -0800

    fixup!

--- prepublish.sh
+++ prepublish.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo Updating loopback-example-app
-rm -rf data loopback-example-app
-mkdir -p data
-git clone --depth=1 --branch=production http://github.com/strongloop/loopback-example-app.git
-mv loopback-example-app data/
+rm -rf data
+mkdir data
+git clone --depth=1 --branch=production \
+  https://github.com/strongloop/loopback-example-app.git \
+  data/loopback-example-app
