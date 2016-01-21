205ce881e66d8fa3bdd4bbecaebdad919aa72810
Krishna Raman <kraman@gmail.com>
2016-01-21 07:35:20 -0800

    WIP: prelim support for plugin based command loading

--- bin/slc.js
+++ bin/slc.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
-require('../lib/loader')
-  .createLoader({
-    manuals: require('path').resolve(__dirname, '..', 'man')
-  })
-  .on('error', function(err) {
-    console.error(err.message);
-    process.exit(1);
-  })
-  .run();
+
+var pluginLoader = require('../lib/plugin-loader');
+var program = require('commander');
+var _ = require('lodash');
+
+var argv = process.argv;
+// var $0 = process.env.CMD ? process.env.CMD : path.basename(argv[1]);
+
+pluginLoader.loadPlugins().then(function(plugins) {
+  _.each(plugins, processPlugins);
+  program.parse(argv);
+});
+
+function processPlugins(plugin) {
+  _.each(plugin.commands, createCommands);
+}
+
+function createCommands(cmd) {
+  var c = program.command(cmd.command);
+  c.helpFileName = cmd.helpFileName;
+  _.each(cmd.aliases, function(alias) {
+    c.alias(alias);
+  });
+  _.each(cmd.options, function(option) {
+    c.option(option.flags, option.description);
+  });
+  c.action(cmd.action);
+}
